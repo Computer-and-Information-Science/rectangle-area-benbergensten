2045b8dd8b789a19327910ed84d61b12c9db1721
benbergensten <benjamin.bergensten@student.northampton.edu>
2026-02-02 03:43:33 +0000

    wrote rectangle area program

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
-
+  double area = length * width;
+  double perimeter = (length * 2) + (width * 2);
+  cout << "Rrectangle Properties: \n";
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+  cout << "Area = " << area << endl;
+  cout << "Perimeter = " << perimeter << endl;
 
 }
